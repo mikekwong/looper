3bb1a13a838c18abe0be21af1e87abe52669a27e
Mike Wong <wongmdesign@gmail.com>
2019-03-20 22:21:42 -0400

    routes_header and base components added

---
+++ client/src/components/App.js
@@ -0,0 +1,27 @@
+import React from 'react'
+import { BrowserRouter, Route } from 'react-router-dom'
+import StreamCreate from './streams/StreamCreate'
+import StreamEdit from './streams/StreamEdit'
+import StreamDelete from './streams/StreamDelete'
+import StreamList from './streams/StreamList'
+import StreamShow from './streams/StreamShow'
+import Header from './Header'
+
+const App = () => {
+  return (
+    <div className='ui container'>
+      <BrowserRouter>
+        <div>
+          <Header />
+          <Route exact path='/' component={StreamList} />
+          <Route exact path='/streams/new' component={StreamCreate} />
+          <Route exact path='/streams/edit' component={StreamEdit} />
+          <Route exact path='/streams/delete' component={StreamDelete} />
+          <Route exact path='/streams/show' component={StreamShow} />
+        </div>
+      </BrowserRouter>
+    </div>
+  )
+}
+
+export default App
